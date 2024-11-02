e72922f0988edf9f826489e9a299d2f06d0ec786
Ara-Yeroyan <ar23yeroyan@gmail.com>
2024-11-02 18:51:35 +0400

    Align with post-VAD manifest format

--- vac_aligner/utils.py
+++ vac_aligner/utils.py
@@ -1,8 +1,35 @@
 import os
+import json
 from typing import Dict
 
+import wave
 
 from .dto import ASRConfig, ALIGNERConfig
+
+
+def add_durations(manifest_path: str):
+    new_path = manifest_path.replace(".json", "_with_durs.json")
+    with open(manifest_path, "r", encoding='utf-8') as f:
+        for line in f:
+            item = json.loads(line)
+            if "duration" not in item:
+                with open(new_path, "r", encoding='utf-8') as w:
+                    audio_path = item['audio_filepath']
+                    duration = get_wav_duration(audio_path)
+                    item['duration'] = duration
+                    w.write(json.dumps(item, ensure_ascii=False) + "\n")
+            else:
+                return
+    print(f"Successfully created a new vad_manifest with durations: {new_path}")
+
+
+def get_wav_duration(file_path):
+    with wave.open(file_path, 'r') as wav_file:
+        frames = wav_file.getnframes()
+        frame_rate = wav_file.getframerate()
+        duration = frames / float(frame_rate)
+
+    return duration
 
 
 def create_nested_folders(path: str):
